d5eeee9c6e53ab02885b413f8537077312e3681b
Hiwoshu <hiwoshu@hotmail.com>
2019-03-07 12:46:02 -0300

    Finish the application

---
+++ src/spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  token: 'BQALlBjRSoyW8nLZAjdAQSytZcmfkCGQoBDCSVgJrO7LW9WmL__SL1b5pKDTExlgDJK2CHR-IvTuwboiAnBNtSqM4tpR-W2jJN5dQgA8gJ3LE3t3ard9CPDGeAGxqXIXfT3LHsAeoBorTrck2g_v4q2cruq7zEbeKQ'
+});
+
+export default spotify;
